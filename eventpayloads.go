77db85ffef5de67fe99fd00ec3e7574ca1dcd54b
Jud White <jud_white@dell.com>
2015-10-29 19:59:56 -0500

    add push event payload; custom date/time deserialization

--- eventpayloads.go
+++ eventpayloads.go
@@ -1,20 +1,77 @@
 package ghapi
 
-type GithubEventPayload struct {
+type GitHubEventPayload struct {
 	Repository RepositoryPayload `json:"repository"`
 	Sender     User              `json:"sender"`
 }
 
 type PullRequestEventPayload struct {
-	GithubEventPayload
+	GitHubEventPayload
 	Action      PullRequestAction  `json:"action"`
 	Number      int                `json:"number"`
 	PullRequest PullRequestPayload `json:"pull_request"`
 }
 
 type IssueCommentEventPayload struct {
-	GithubEventPayload
+	GitHubEventPayload
 	Action  string              `json:"action"`
 	Issue   IssuePayload        `json:"issue"`
 	Comment IssueCommentPayload `json:"comment"`
+}
+
+type Pusher struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type PushEventPayload struct {
+	GitHubEventPayload
+	Ref     string  `json:"ref"`
+	Before  string  `json:"before"`
+	After   string  `json:"after"`
+	Created bool    `json:"created"`
+	Deleted bool    `json:"deleted"`
+	Forced  bool    `json:"forced"`
+	BaseRef *string `json:"base_ref"`
+	Compare string  `json:"compare"`
+	Commits []struct {
+		ID        string `json:"id"`
+		Distinct  bool   `json:"distinct"`
+		Message   string `json:"message"`
+		Timestamp string `json:"timestamp"`
+		URL       string `json:"url"`
+		Author    struct {
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Username string `json:"username"`
+		} `json:"author"`
+		Committer struct {
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Username string `json:"username"`
+		} `json:"committer"`
+		Added    []string `json:"added"`
+		Removed  []string `json:"removed"`
+		Modified []string `json:"modified"`
+	} `json:"commits"`
+	HeadCommit struct {
+		ID        string `json:"id"`
+		Distinct  bool   `json:"distinct"`
+		Message   string `json:"message"`
+		Timestamp string `json:"timestamp"`
+		URL       string `json:"url"`
+		Author    struct {
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Username string `json:"username"`
+		} `json:"author"`
+		Committer struct {
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Username string `json:"username"`
+		} `json:"committer"`
+		Added    []string `json:"added"`
+		Removed  []string `json:"removed"`
+		Modified []string `json:"modified"`
+	} `json:"head_commit"`
 }
